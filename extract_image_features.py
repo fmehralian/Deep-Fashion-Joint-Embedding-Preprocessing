01e8bef03dfad9d30790abe7a8fae567a85d9316
Forough <forough@Foroughs-MacBook-Pro.local>
2020-03-01 09:39:22 -0800

    update data path

--- extract_image_features.py
+++ extract_image_features.py
@@ -5,10 +5,13 @@ from img_to_vec import Img2Vec
 from PIL import Image
 
 img2vec = Img2Vec(cuda=True)
+version = 1
+
 
 def ExtractImageFeature(path):
 	img = Image.open(path)
 	return img2vec.get_vec(img)	
+
 
 def SaveDict(data):
 	import pickle	
@@ -17,10 +20,11 @@ def SaveDict(data):
 		pickle.dump(data, f, pickle.HIGHEST_PROTOCOL)
 		print("saved!")
 
+
 if __name__ == "__main__":
 
 	# Where we will search for images
-	path_to_images = "In-shop Clothes Retrieval Benchmark/" #sys.argv[1]
+	path_to_images = "data/selected_v{}/".format(version) #sys.argv[1]
 
 	# Dictionary to store results
 	result = {}
@@ -28,7 +32,7 @@ if __name__ == "__main__":
 	idx = 0
 
 	# Search all sub directories
-	for filename in glob.iglob(path_to_images+"**/*/", recursive=True):	
+	for filename in glob.iglob(path_to_images+"*"):
 
 		idx += 1
 
@@ -36,19 +40,18 @@ if __name__ == "__main__":
 		files = (glob.glob(filename+"*.jpg") + glob.glob(filename+"*.png"))
 
 		# Create array of extracted image features
-		vectors = [ExtractImageFeature(file_path) for file_path in files]
+		vectors = ExtractImageFeature(filename)
 
 		# Get "image id" of folder
-		image_id = filename.rsplit('/')[-2]
+		image_id = filename.split('/')[-1].split("_")[0]
 
 		# Check it is a valid ID
-		if "id_" in image_id:
-			# If so, let's map this image id to image vectors.
-			print(image_id)		
-			result[image_id] = vectors	
 
-			if idx % 50 == 0:
-				SaveDict(result)
+		print(image_id)
+		result[image_id] = vectors
+
+		if idx % 50 == 0:
+			SaveDict(result)
 
 	# Save final dictionary
 	SaveDict(result)
