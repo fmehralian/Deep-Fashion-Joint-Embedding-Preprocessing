7ccc2b924016de13dd501bb80ac0cdc1cdaed309
fmehralian <f.mehralian90@gmail.com>
2020-03-13 15:07:02 -0700

    pass file name in dataloader

--- auto_encoder.py
+++ auto_encoder.py
@@ -4,7 +4,7 @@ from PIL import Image
 from torch import nn
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
-from torchvision import transforms
+from torchvision import transforms, datasets
 from torchvision.datasets import MNIST
 from torchvision.utils import save_image
 import os
@@ -35,12 +35,26 @@ img_transform = transforms.Compose([
 
 path_to_images = "data/bw_icons_v{}/".format(version)
 
+class ImageFolderWithPaths(datasets.ImageFolder):
+    """Custom dataset that includes image file paths. Extends
+    torchvision.datasets.ImageFolder
+    """
+
+    # override the __getitem__ method. this is the method that dataloader calls
+    def __getitem__(self, index):
+        # this is what ImageFolder normally returns
+        original_tuple = super(ImageFolderWithPaths, self).__getitem__(index)
+        # the image file path
+        path = self.imgs[index][0]
+        # make a new tuple that includes original and the path
+        tuple_with_path = (original_tuple + (path,))
+        return tuple_with_path
 
 def load_dataset():
     data_path = 'data/bw_icons_v1/'
-    train_dataset = torchvision.datasets.ImageFolder(
+    train_dataset = ImageFolderWithPaths(
         root=data_path,
-        transform=torchvision.transforms.ToTensor()
+        transform=img_transform #torchvision.transforms.ToTensor()
     )
     train_loader = torch.utils.data.DataLoader(
         train_dataset,
@@ -76,22 +90,22 @@ class autoencoder(nn.Module):
         )
 
     def forward(self, x):
-        x = self.encoder(x)
-        x = self.decoder(x)
-        return x
+        encoded = self.encoder(x)
+        decoded = self.decoder(encoded)
+        return encoded, decoded
 
 
-model = autoencoder().cuda()
+model = autoencoder()
 criterion = nn.MSELoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate,
                              weight_decay=1e-5)
 
 for epoch in range(num_epochs):
-    for batch_idx, (data, target) in enumerate(load_dataset()):
+    for batch_idx, (data, labels, paths) in enumerate(load_dataset()):
         img = data[:,0,:,:].unsqueeze(1)
-        img = Variable(img).cuda()
+        img = Variable(img)
         # ===================forward=====================
-        output = model(img)
+        enc, output = model(img)
         loss = criterion(output, img)
         # ===================backward====================
         optimizer.zero_grad()
